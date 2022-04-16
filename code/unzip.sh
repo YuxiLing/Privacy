183da36f06a679b39aad1fc9c9a3b13d07a88a82
YuxiLing <69611264+YuxiLing@users.noreply.github.com>
2022-04-16 16:10:33 +0000

    Update unzip.sh

--- code/unzip.sh
+++ code/unzip.sh
@@ -9,11 +9,12 @@
 # $1 is the root folder of extensions
 # Example: $1="$HOME/Desktop/VerifiedMalicious"
 
-newpath="$1/unzip/$2"
+newpath="$1/unzip"
+rm -r $newpath
 mkdir $newpath
 
-path="$1/zip/$2"
-files=$(ls $path)
+path="$1/zip"
+files=$(ls $path/[c-d]*.crx | xargs -n 1 basename)
 echo "start to unzip folder $path"
 for filename in $files
 do
